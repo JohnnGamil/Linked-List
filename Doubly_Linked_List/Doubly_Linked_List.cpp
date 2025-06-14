3b4af8290170acbf5d8091337d3b8e07ff39e55b
JohnnGamil <johnngamil@gmail.com>
2025-06-15 02:34:32 +0300

    Added some functions

--- Doubly_Linked_List/Doubly_Linked_List.cpp
+++ Doubly_Linked_List/Doubly_Linked_List.cpp
@@ -44,6 +44,53 @@ void backwardTraversal(Node* head){
 }
 
 
+void insertAtStart(Node* &head, int v){
+    Node* newNode = new Node(v);
+    if(head==nullptr){
+        head = newNode;
+        return;
+    }
+    newNode->next = head;
+    head->prev = newNode;
+    head = newNode;
+}
+
+
+void insertAtEnd(Node* &head, int v){
+    if(head == nullptr){
+        insertAtStart(head, v);
+        return;
+    }
+    Node* newNode = new Node(v);
+    Node* lastNode = head;
+    while(lastNode->next){
+        lastNode = lastNode->next;
+    }
+    lastNode->next = newNode;
+    newNode->prev = lastNode;
+}
+
+
+void insertAtMiddle(Node* &head, int v, int k){
+    if(k==1){
+        insertAtStart(head, v);
+        return;
+    }
+    Node* temp = head;
+    for(int i=1;i<k-1;i++){
+        temp=temp->next;
+    }
+    Node* newNode = new Node(v);
+    if(temp->next){
+         temp->next->prev = newNode;
+    }
+    newNode->next = temp->next;
+    temp->next = newNode;
+    newNode->prev = temp;
+}
+
+
+
 int main(){
     Node* n1 = new Node(1);    
     Node* n2 = new Node(2);
@@ -52,4 +99,9 @@ int main(){
     Node* head = n1;
     forwardTravesal(head);
     backwardTraversal(head);
+    insertAtStart(head,9);
+    forwardTravesal(head);
+    insertAtMiddle(head,3,1);
+    forwardTravesal(head);
+
 }
